ad0fe295dbf4f02401bc41908cf3b2e63b964914
hajime.sueyoshi <hajime.sueyoshi@classi.jp>
2018-11-30 17:32:54 +0900

    refactor test

--- src/test/scala/example/MoneyTest.scala
+++ src/test/scala/example/MoneyTest.scala
@@ -3,6 +3,10 @@ package example
 import org.scalatest._
 
 class MoneyTest extends FunSuite {
+  val bank: Bank = new Bank()
+  val fiveBucks: Expression = Money.dollar(5)
+  val tenFrancs: Expression = Money.franc(10)
+
   test("Multiplication") {
     var five: Money = Money.dollar(5)
     assert(five.times(2) == Money.dollar(10))
@@ -24,7 +28,6 @@ class MoneyTest extends FunSuite {
     val five: Money = Money.dollar(5)
     val sum: Expression = five.plus(five)
 
-    val bank: Bank = new Bank()
     val reduced: Money = bank.reduce(sum, "USD")
     assert(reduced == Money.dollar(10))
   }
@@ -39,19 +42,16 @@ class MoneyTest extends FunSuite {
 
   test("Reduce Sum") {
     val sum: Expression = new Sum(Money.dollar(3), Money.dollar(4))
-    val bank: Bank = new Bank
     val result: Money = bank.reduce(sum, "USD")
     assert(result == Money.dollar(7))
   }
 
   test("Reduce Money") {
-    val bank: Bank = new Bank
     val result: Money = bank.reduce(Money.dollar(1), "USD")
     assert(result == Money.dollar(1))
   }
 
   test("Reduce Money Different Currency") {
-    val bank: Bank = new Bank
     bank.addRate("CHF", "USD", 2)
 
     val result: Money = bank.reduce(Money.franc(2), "USD")
@@ -63,18 +63,12 @@ class MoneyTest extends FunSuite {
   }
 
   test("Mixed Addition") {
-    val fiveBucks: Expression = Money.dollar(5)
-    val tenFrancs: Expression = Money.franc(10)
-    val bank: Bank = new Bank
     bank.addRate("CHF", "USD", 2)
     val result: Money = bank.reduce(fiveBucks.plus(tenFrancs), "USD")
     assert(result == Money.dollar(10))
   }
 
   test("Sum Plus Money") {
-    val fiveBucks: Expression = Money.dollar(5)
-    val tenFrancs: Expression = Money.franc(10)
-    val bank: Bank = new Bank
     bank.addRate("CHF", "USD", 2)
     val sum: Expression = new Sum(fiveBucks, tenFrancs).plus(fiveBucks)
     val result: Money = bank.reduce(sum, "USD")
@@ -82,9 +76,6 @@ class MoneyTest extends FunSuite {
   }
 
   test("Sum Times") {
-    val fiveBucks: Expression = Money.dollar(5)
-    val tenFrancs: Expression = Money.franc(10)
-    val bank: Bank = new Bank
     bank.addRate("CHF", "USD", 2)
     val sum: Expression = new Sum(fiveBucks, tenFrancs).times(2)
     val result: Money = bank.reduce(sum, "USD")
